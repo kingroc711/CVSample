4200961cd8a2e4306f72e7c03e94bb282714311c
king <960889@qq.com>
2018-10-12 16:23:32 +0800

    add 0-SystemInfo.py

--- OpenCV/5-ImageResize.py
+++ OpenCV/5-ImageResize.py
@@ -3,7 +3,7 @@ import cv2
 img = cv2.imread('./res/aero3.jpg')
 print(img.shape[:2])
 
-width, height = img.shape[:2]
+height, width = img.shape[:2]
 
 reSize1 = cv2.resize(img, (2*width, 2*height), interpolation=cv2.INTER_CUBIC)
 reSize2 = cv2.resize(img, (int(width/2), int(height/2)), interpolation=cv2.INTER_CUBIC)
--- OpenCV/6-ImageRotation.py
+++ OpenCV/6-ImageRotation.py
@@ -3,10 +3,10 @@ import cv2
 img = cv2.imread('./res/aero3.jpg')
 print(img.shape[:2])
 
-width, height = img.shape[:2]
+height, width = img.shape[:2]
 
-M = cv2.getRotationMatrix2D((width/2, height/2), 45, 1)
-img_ro = cv2.warpAffine(img, M, img.shape[:2])
+M = cv2.getRotationMatrix2D((width/2, height/2), 90*2, 1)
+img_ro = cv2.warpAffine(img, M, (width, height))
 
 cv2.imshow('rotation', img_ro)
 
