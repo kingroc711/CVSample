f3cd1d689139b27362fad5c43b4151abd3a30898
king <960889@qq.com>
2018-11-29 11:34:39 +0800

    add 10-ImageFilp.py

---
+++ OpenCV/10-ImageFilp.py
@@ -0,0 +1,14 @@
+import cv2
+
+
+img = cv2.imread('./res/mini.jpeg')
+imgH = cv2.flip(img, 1)
+imgV = cv2.flip(img, 0)
+imgHV = cv2.flip(img, -1)
+
+cv2.imshow('img', img)
+cv2.imshow('imgH', imgH)
+cv2.imshow('imgV', imgV)
+cv2.imshow('imgHV', imgHV)
+cv2.waitKey()
+cv2.destroyAllWindows()
