e30dbc4d845325a2eddbf08802fb385c7d00c725
king <960889@qq.com>
2018-12-03 16:49:02 +0800

    add 10-ImageFilp.py

--- OpenCV/18-AdaptThresholding.py
+++ OpenCV/18-AdaptThresholding.py
@@ -4,10 +4,8 @@ from matplotlib import pyplot as plt
 img = cv.imread('./res/CarID.jpeg',0)
 img = cv.medianBlur(img,5)
 ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
-th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
-            cv.THRESH_BINARY,11,2)
-th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,11,2)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
 titles = ['Original Image', 'Global Thresholding (v = 127)',
             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
 images = [img, th1, th2, th3]
