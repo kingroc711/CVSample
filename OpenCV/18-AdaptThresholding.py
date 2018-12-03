9c9cb96c3272dae7386771520899ecbf4bafe3e6
king <960889@qq.com>
2018-12-03 16:48:20 +0800

    add 18-AdaptThresholding.py

---
+++ OpenCV/18-AdaptThresholding.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('./res/CarID.jpeg',0)
+img = cv.medianBlur(img,5)
+ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+            cv.THRESH_BINARY,11,2)
+th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
