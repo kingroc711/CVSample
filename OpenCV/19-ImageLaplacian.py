5a8538097bfd5a13343693db2b9c49fc8257a4e2
king <960889@qq.com>
2018-12-04 11:57:16 +0800

    add 19-ImageLaplacian.py

---
+++ OpenCV/19-ImageLaplacian.py
@@ -0,0 +1,23 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('./res/CarID.jpeg',0)
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+
+soblexy = sobelx + sobely
+
+titles = ['Original', 'Laplacian', 'SobelX', 'SobelY', 'SobelXY']
+imgs = [img, laplacian, sobelx, sobely, soblexy]
+
+plt.figure(figsize = (10, 5))
+
+for i in range(5):
+    plt.subplot(2,3,i+1)
+    plt.imshow(imgs[i], 'gray')
+    plt.title(titles[i])
+    plt.axis('off')
+
+plt.show()
