63d7a2d020a0170737360cf796d575cf69631b43
king <960889@qq.com>
2018-12-04 15:27:38 +0800

    add 20-Canny.py

---
+++ OpenCV/20-Canny.py
@@ -0,0 +1,18 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('./res/mini.jpeg',0)
+edges = cv2.Canny(img,100,200)
+plt.figure(figsize = (20, 10))
+
+plt.subplot(1, 2, 1)
+plt.imshow(img,cmap = 'gray')
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image')
+plt.axis('off')
+
+plt.show()
