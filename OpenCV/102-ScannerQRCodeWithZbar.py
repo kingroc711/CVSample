3cb0b5befe3990356f6974c9411550a8d76e6846
king <960889@qq.com>
2018-12-28 15:18:30 +0800

    update test.mp4

--- OpenCV/102-ScannerQRCodeWithZbar.py
+++ OpenCV/102-ScannerQRCodeWithZbar.py
@@ -5,10 +5,10 @@ import cv2
 
 
 def decode(im):
-    # Find barcodes and QR codes
+    # 在这里查找二维码
     decodedObjects = pyzbar.decode(im)
 
-    # Print results
+    # 打印出结果
     for obj in decodedObjects:
         print('Type : ', obj.type)
         print('Data : ', obj.data, '\n')
@@ -16,27 +16,23 @@ def decode(im):
     return decodedObjects
 
 
-# Display barcode and QR code location
 def display(im, decodedObjects):
-    # Loop over all decoded objects
+
     for decodedObject in decodedObjects:
         points = decodedObject.polygon
 
-        # If the points do not form a quad, find convex hull
         if len(points) > 4:
             hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
             hull = list(map(tuple, np.squeeze(hull)))
         else:
             hull = points;
 
-        # Number of points in the convex hull
+
         n = len(hull)
 
-        # Draw the convext hull
         for j in range(0, n):
             cv2.line(im, hull[j], hull[(j + 1) % n], (255, 0, 0), 3)
 
-    # Display results
     cv2.imshow("Results", im);
     cv2.waitKey(0);
 
