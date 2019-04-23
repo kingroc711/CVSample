00c16657e7d60a5b7881d574a31a5a4e371b9dd6
king <960889@qq.com>
2019-04-23 15:50:02 +0800

    update test.mp4

--- OpenCV/33-ShowText.py
+++ OpenCV/33-ShowText.py
@@ -2,12 +2,13 @@ import cv2
 
 img = cv2.imread('./res/aero3.jpg')
 
-cv2.putText(img, 'Hello World', (0,40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
-cv2.putText(img, 'Hello World', (0,80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
-cv2.putText(img, 'Hello World', (0,120), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 2)
-cv2.putText(img, 'Hello World', (0,160), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
-cv2.putText(img, 'Hello World', (0,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-cv2.putText(img, 'Hello World', (0,240), cv2.FONT_HERSHEY_TRIPLEX, 1, (150, 0, 180), 1)
+#           照片   /添加的文字    /左下角坐标  /字体                            /字体大小 /颜色            /字体粗细
+cv2.putText(img, 'Hello World', (0,40),     cv2.FONT_HERSHEY_COMPLEX,       1,       (0, 255, 0),     1)
+cv2.putText(img, 'Hello World', (0,80),     cv2.FONT_HERSHEY_PLAIN,         1,       (0, 0, 255),     1)
+cv2.putText(img, 'Hello World', (0,120),    cv2.FONT_HERSHEY_COMPLEX_SMALL, 1,       (255, 0, 0),     2)
+cv2.putText(img, 'Hello World', (0,160),    cv2.FONT_HERSHEY_DUPLEX,        1,       (255, 255, 255), 1)
+cv2.putText(img, 'Hello World', (0,200),    cv2.FONT_HERSHEY_SIMPLEX,       1,       (0, 0, 0),       1)
+cv2.putText(img, 'Hello World', (0,240),    cv2.FONT_HERSHEY_TRIPLEX,       1,       (150, 0, 180),   1)
 
 cv2.imshow('imgHV', img)
 
