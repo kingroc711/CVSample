47925d78c0785ef8d7b9b359b8a24cfd8ef4fa71
king <960889@qq.com>
2019-04-23 15:41:16 +0800

    update test.mp4

---
+++ OpenCV/33-ShowText.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('./res/aero3.jpg')
+
+cv2.putText(img, 'Hello World', (0,40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
+cv2.putText(img, 'Hello World', (0,80), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+cv2.putText(img, 'Hello World', (0,120), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 2)
+cv2.putText(img, 'Hello World', (0,160), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
+cv2.putText(img, 'Hello World', (0,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+cv2.putText(img, 'Hello World', (0,240), cv2.FONT_HERSHEY_TRIPLEX, 1, (150, 0, 180), 1)
+
+cv2.imshow('imgHV', img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
