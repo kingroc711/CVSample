2f582497bc3873e3181032bdf4ba2cab27dbb425
king <960889@qq.com>
2019-04-23 16:24:58 +0800

    update test.mp4

--- OpenCV/106.py
+++ OpenCV/106.py
@@ -1,25 +1,44 @@
-import cv2 as cv
-from cv2 import dnn
+import cv2
+import numpy as np
+
+videoLeftUp = cv2.VideoCapture('./res/2_003_013.mp4')
+videoLeftDown = cv2.VideoCapture('./res/2_003_014.mp4')
+videoRightUp = cv2.VideoCapture('./res/2_003_015.mp4')
+videoRightDown = cv2.VideoCapture('./res/2_003_016.mp4')
+
+fps = videoLeftUp.get(cv2.CAP_PROP_FPS)
+
+width = (int(videoLeftUp.get(cv2.CAP_PROP_FRAME_WIDTH)))
+height = (int(videoLeftUp.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+#videoWriter = cv2.VideoWriter('./out/4in1.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (width, height))
+
+successLeftUp, frameLeftUp = videoLeftUp.read()
+successLeftDown , frameLeftDown = videoLeftDown.read()
+successRightUp, frameRightUp = videoRightUp.read()
+successRightDown, frameRightDown = videoRightDown.read()
+
+while successLeftUp and successLeftDown and successRightUp and successRightDown:
+    frameLeftUp = cv2.resize(frameLeftUp, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+    frameLeftDown = cv2.resize(frameLeftDown, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+    frameRightUp = cv2.resize(frameRightUp, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+    frameRightDown = cv2.resize(frameRightDown, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+
+    frameUp = np.hstack((frameLeftUp, frameRightUp))
+    frameDown = np.hstack((frameLeftDown, frameRightDown))
+    frame = np.vstack((frameUp, frameDown))
 
 
-img = cv.imread('/home/king/Desktop/1.png')
-net = dnn.readNetFromCaffe('./res/deploy.prototxt', './res/res10_300x300_ssd_iter_140000.caffemodel')
+    if cv2.waitKey(1) == 27:
+        break
+    cv2.imshow('Test camera', frame)
+    successLeftUp, frameLeftUp = videoLeftUp.read()
+    successLeftDown, frameLeftDown = videoLeftDown.read()
+    successRightUp, frameRightUp = videoRightUp.read()
+    successRightDown, frameRightDown = videoRightDown.read()
 
-net.setInput(dnn.blobFromImage(img, 1.0, (300, 300), (104.0, 177.0, 123.0), False, False))
-detections = net.forward()
-
-cols = img.shape[1]
-rows = img.shape[0]
-
-for i in range(detections.shape[2]):
-    confidence = detections[0, 0, i, 2]
-    if confidence > 0.5:
-        xLeftBottom = int(detections[0, 0, i, 3] * cols)
-        yLeftBottom = int(detections[0, 0, i, 4] * rows)
-        xRightTop = int(detections[0, 0, i, 5] * cols)
-        yRightTop = int(detections[0, 0, i, 6] * rows)
-
-        cv.rectangle(img, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop), (0, 0, 255))
-cv.imshow('img', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+#videoWriter.release()
+videoLeftUp.release()
+videoLeftDown.release()
+videoRightUp.release()
+videoRightDown.release()
