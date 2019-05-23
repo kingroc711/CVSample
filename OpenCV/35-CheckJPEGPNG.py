c55892df5c1b0ad2994fbec11a399e94c6986227
king <960889@qq.com>
2019-05-23 10:24:54 +0800

    update

--- OpenCV/35-CheckJPEGPNG.py
+++ OpenCV/35-CheckJPEGPNG.py
@@ -1,23 +1,42 @@
 import os
+import cv2
 
-path = './res/'
+DirList = [
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/drawings',
+    # '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/hentai',
+    # '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/neutral',
+    # '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/porn',
+    # '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/sexy'
+]
+
 
 def is_valid_jpg(jpg_file):
     with open(jpg_file, 'rb') as f:
         f.seek(-2, 2)
         buf = f.read()
-        return buf ==  b'\xff\xd9'
+        return buf == b'\xff\xd9'
+
 
 def is_valid_png(png_file):
     with open(png_file, 'rb') as f:
-        f.seek(-2, 2)
+        f.seek(-3, 2)
         buf = f.read()
-        return buf == b'\x60\x82'
+        if buf == b'\x60\x82\x00':
+            return True
+        elif buf[1:] == b'\x60\x82':
+            return True
+        else:
+            return False
 
-for file in os.listdir('res'):
-    pic_file = path + file
-    isJpg = is_valid_jpg(pic_file)
-    isPng = is_valid_png(pic_file)
+for path in DirList:
+    for file in os.listdir(path):
+        pic_file = os.path.join(path, file)
+        if pic_file.endswith('jpeg'):
+            isJpg = is_valid_jpg(pic_file)
+            if isJpg == False:
+                print(pic_file)
 
-
-    print("jpeg : %s, png %s, file %s " % (isJpg, isPng, file))
+        if pic_file.endswith('png'):
+            isPng = is_valid_png(pic_file)
+            if isPng == False:
+                print(pic_file)
