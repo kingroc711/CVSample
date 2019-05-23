5303d061a6458d1e70513a0857b95717e85e5ba3
king <960889@qq.com>
2019-05-23 14:00:56 +0800

    update

---
+++ Python/PNGConvertJPEG.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+DirList = [
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/drawings',
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/hentai',
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/neutral',
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/porn',
+    '/home/king/PycharmProjects/nsfw_data_scrapper/raw_data/sexy'
+]
+
+for path in DirList:
+    for filename in os.listdir(path):
+        fullName = os.path.join(path, filename)
+        if fullName.endswith('.png'):
+            mainName, ext = os.path.splitext(fullName)
+            print(fullName, mainName + '.jpeg')
+            im = Image.open(fullName)
+            bg = Image.new('RGB', im.size, (255, 255, 255))
+            bg.paste(im, (0, 0), im)
+            bg.save(mainName + '.jpeg', quality=99)
+            os.remove(fullName)
