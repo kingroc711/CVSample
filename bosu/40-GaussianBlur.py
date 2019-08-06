ebd69a4396e82dc8c607e53406d9c22825952e57
king711 <960889@qq.com>
2019-08-06 19:58:38 +0800

    update

--- bosu/40-GaussianBlur.py
+++ bosu/40-GaussianBlur.py
@@ -16,56 +16,63 @@ def createHtml():
     html_start = '<html>\n' \
                  '<style>\n' \
                  'img{\n' \
-                 'width: 200px;\n' \
+                 'width: 400px;\n' \
                  'height: auto;\n' \
                  '}\n' \
                  '</style>\n\n' \
                  '<body>\n\n'
+    os.remove('./a.html')
     html = open('./a.html', 'w')
     html.write(html_start)
     return html
 
-def getBigCircle(file):
+def getBigCircle(file, downthreshold=0):
     img = cv.imread(file, cv.IMREAD_COLOR)
     height, width = img.shape[:2]
-    print("height : " + str(height) + ', width : ' + str(width))
+    #print("height : " + str(height) + ', width : ' + str(width))
 
     img = cv.resize(img, (int(width / 2), int(height / 2)))
     cimg = img.copy()
-    img = cv.medianBlur(img, 3)
+    img = cv.medianBlur(img, 5)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.GaussianBlur(img, (5, 5), 0, 0)
-    img = cv.Canny(img, 50, 200)
-    # img = cv.GaussianBlur(img, (9, 9), 2, 2)
-    #cv.imshow('blur', img)
-
-    circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, int(height / 2), np.array([]), param1=100, param2=30,
-                              minRadius=int(height / 8), maxRadius=int(height / 2))
+    img = cv.Canny(img, 50 - downthreshold, 100 - downthreshold)
+    circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, int(height / 2), np.array([]), param1=100, param2=50,
+                              minRadius=int(height / 8), maxRadius=int(height / 4))
     circles = np.uint16(np.around(circles))
-    print(circles)
+    print(circles, file)
     if len(circles) is not 0:
         for i in circles[0, :]:
-            # draw the outer circle
-            cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # draw the center of the circle
-            cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+            if i[0] - i[2] > 0 and i[0] + i[2] < width / 2 and i[1] - i[2] > 0 and i[1] + i[2] < height / 2:
+                # draw the outer circle
+                cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                # draw the center of the circle
+                cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+            else:
+                return None
+    else:
+        return None
 
-    #cv.imshow('circles', cimg)
-    #cv.waitKey()
-    #cv.destroyAllWindows()
     return cimg
 
 def main():
     html = createHtml()
-    path = './buliang'
+    path = './liepian'
     for filename in os.listdir(path):
         fullName = os.path.join(path, filename)
-        img = getBigCircle(fullName)
-        cv.imwrite(os.path.join('./out', filename), img)
-        html.write(div_string % (fullName, os.path.join('./out', filename)))
+        global img
+        for i in range(3):
+            img = getBigCircle(fullName, downthreshold=20*i)
+            if img is not None:
+                cv.imwrite(os.path.join('./out', filename), img)
+                html.write(div_string % (fullName, os.path.join('./out', filename)))
+                break
+        if img is None:
+            html.write(div_string % (fullName, fullName))
 
     html.write(html_end)
     html.close()
+    cv.waitKey()
 
 if __name__ == '__main__':
     main()
