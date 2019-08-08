f75520817038da5e429552bda5317e0e12e326b4
king711 <960889@qq.com>
2019-08-08 11:39:41 +0800

    update

--- bosu/40-GaussianBlur.py
+++ bosu/40-GaussianBlur.py
@@ -12,7 +12,7 @@ div_string = '<div>\n' \
 
 
 
-def createHtml():
+def createHtml(path):
     html_start = '<html>\n' \
                  '<style>\n' \
                  'img{\n' \
@@ -21,8 +21,7 @@ def createHtml():
                  '}\n' \
                  '</style>\n\n' \
                  '<body>\n\n'
-    os.remove('./a.html')
-    html = open('./a.html', 'w')
+    html = open(path + '.html', 'w')
     html.write(html_start)
     return html
 
@@ -33,17 +32,17 @@ def getBigCircle(file, downthreshold=0):
 
     img = cv.resize(img, (int(width / 2), int(height / 2)))
     cimg = img.copy()
-    img = cv.medianBlur(img, 5)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img = cv.GaussianBlur(img, (5, 5), 0, 0)
-    img = cv.Canny(img, 50 - downthreshold, 100 - downthreshold)
+    img = cv.medianBlur(img, 7)
+    #img = cv.GaussianBlur(img, (5, 5), 0, 0)
+    img = cv.Canny(img, 100 - downthreshold/2, 200 - downthreshold)
     circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, int(height / 2), np.array([]), param1=100, param2=50,
                               minRadius=int(height / 8), maxRadius=int(height / 4))
     circles = np.uint16(np.around(circles))
     print(circles, file)
     if len(circles) is not 0:
         for i in circles[0, :]:
-            if i[0] - i[2] > 0 and i[0] + i[2] < width / 2 and i[1] - i[2] > 0 and i[1] + i[2] < height / 2:
+            if int(i[0]) - int(i[2]) > 0 and int(i[0]) + int(i[2]) < width / 2 and int(i[1]) - int(i[2]) > 0 and int(i[1]) + int(i[2]) < height / 2:
                 # draw the outer circle
                 cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
                 # draw the center of the circle
@@ -56,13 +55,13 @@ def getBigCircle(file, downthreshold=0):
     return cimg
 
 def main():
-    html = createHtml()
-    path = './liepian'
+    path = './quejiao'
+    html = createHtml(path)
     for filename in os.listdir(path):
         fullName = os.path.join(path, filename)
         global img
-        for i in range(3):
-            img = getBigCircle(fullName, downthreshold=20*i)
+        for i in range(4):
+            img = getBigCircle(fullName, downthreshold=40*i)
             if img is not None:
                 cv.imwrite(os.path.join('./out', filename), img)
                 html.write(div_string % (fullName, os.path.join('./out', filename)))
