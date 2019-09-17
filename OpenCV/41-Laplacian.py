b275b48440254e97645fa52279f6fabd2213c7bb
king711 <960889@qq.com>
2019-09-17 15:03:13 +0800

    update

--- OpenCV/41-Laplacian.py
+++ OpenCV/41-Laplacian.py
@@ -1,9 +1,32 @@
 import cv2 as cv
+import numpy as np
 
 fileList = ['res/mohu.jpg', 'res/mohu1.jpg', 'res/mohu2.jpg',
             'res/mohu3.png', 'res/mediablur.png', 'res/jeep.jpeg']
 
 for f in fileList:
-    grayImag = cv.imread(f, 0)
-    imageVar = cv.Laplacian(grayImag, cv.CV_64F).var()
-    print(imageVar)
+    imag = cv.imread(f)
+    grayImag = cv.cvtColor(imag, cv.COLOR_BGR2GRAY)
+    #grayImag = cv.GaussianBlur(grayImag, (3, 3), 0)
+    canny = cv.Canny(grayImag, 200, 200)
+    value = canny.var()
+    print('Canny : ' + str(value))
+
+    lapla = cv.Laplacian(grayImag, cv.CV_8U)
+    imageVar = lapla.var()
+    print('Laplacian : ' + str(imageVar))
+
+    grayImag = cv.cvtColor(grayImag, cv.COLOR_GRAY2BGR)
+    canny = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
+    cv.putText(canny, str(value), (0, 40), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+
+    lapla = cv.cvtColor(lapla, cv.COLOR_GRAY2BGR)
+    cv.putText(lapla, str(imageVar), (0, 40), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
+
+    longimag = np.hstack((imag, canny, lapla))
+    cv.namedWindow(f, 0)
+    cv.imshow(f, longimag)
+
+while(1):
+    if(cv.waitKey(100)==27):
+        break
