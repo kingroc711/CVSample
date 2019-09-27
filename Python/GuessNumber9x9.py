1284cbe4d139e9b91490d42485cc4360b5733cf1
king711 <960889@qq.com>
2019-09-27 18:15:17 +0800

    update

--- Python/GuessNumber9x9.py
+++ Python/GuessNumber9x9.py
@@ -17,15 +17,30 @@ def printMat(mat):
     print()
 
 mainMat = np.zeros((H,W), dtype=np.int8)
-mainMat[0, 1] = 7; mainMat[0, 5] = 5; mainMat[0, 7] = 9
-mainMat[1, 0] = 4; mainMat[1, 5] = 6; mainMat[1, 8] = 7
-mainMat[2, 3] = 8; mainMat[2, 5] = 2
-mainMat[3, 0] = 2; mainMat[3, 1] = 8; mainMat[3, 2] = 6; mainMat[3, 6] = 4
-mainMat[5, 2] = 9; mainMat[5, 6] = 1; mainMat[5, 7] = 7; mainMat[5, 8] = 8
-mainMat[6, 3] = 4; mainMat[6, 5] = 1
-mainMat[7, 0] = 3; mainMat[7, 3] = 6; mainMat[7, 8] = 5
-mainMat[8, 1] = 4; mainMat[8, 3] = 3; mainMat[8, 7] = 2
+if False:
+    mainMat[0, 1] = 7; mainMat[0, 5] = 5; mainMat[0, 7] = 9
+    mainMat[1, 0] = 4; mainMat[1, 5] = 6; mainMat[1, 8] = 7
+    mainMat[2, 3] = 8; mainMat[2, 5] = 2
+    mainMat[3, 0] = 2; mainMat[3, 1] = 8; mainMat[3, 2] = 6; mainMat[3, 6] = 4
+    mainMat[5, 2] = 9; mainMat[5, 6] = 1; mainMat[5, 7] = 7; mainMat[5, 8] = 8
+    mainMat[6, 3] = 4; mainMat[6, 5] = 1
+    mainMat[7, 0] = 3; mainMat[7, 3] = 6; mainMat[7, 8] = 5
+    mainMat[8, 1] = 4; mainMat[8, 3] = 3; mainMat[8, 7] = 2
+else:
+    mainMat[0, 0] = 5; mainMat[0, 1] = 3; mainMat[0, 4] = 7
+    mainMat[1, 0] = 6; mainMat[1, 3] = 1; mainMat[1, 4] = 9; mainMat[1, 5] = 5
+    mainMat[2, 1] = 9; mainMat[2, 2] = 8; mainMat[2, 7] = 6
+
+    mainMat[3, 0] = 8; mainMat[3, 4] = 6; mainMat[3, 8] = 3
+    mainMat[4, 0] = 4; mainMat[4, 3] = 8; mainMat[4, 5] = 3; mainMat[4, 8] = 1
+    mainMat[5, 0] = 7; mainMat[5, 4] = 2; mainMat[5, 8] = 6
+
+    mainMat[6, 1] = 6; mainMat[6, 6] = 2; mainMat[6, 7] = 8
+    mainMat[7, 3] = 4; mainMat[7, 4] = 1; mainMat[7, 5] = 9; mainMat[7, 8] = 5
+    mainMat[8, 4] = 8; mainMat[8, 7] = 7; mainMat[8, 8] = 9
+
 printMat(mainMat)
+
 
 lastMat = np.zeros(mainMat.shape, mainMat.dtype)
 
