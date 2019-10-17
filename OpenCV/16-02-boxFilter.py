93be1b6e82f50e5ee9a6d0b00658cf616bb10185
king711 <960889@qq.com>
2019-10-17 15:39:46 +0800

    update

--- OpenCV/16-02-boxFilter.py
+++ OpenCV/16-02-boxFilter.py
@@ -5,7 +5,7 @@ cv2.imshow('org', img)
 kernelsizes = [(3,3), (9,9), (15, 15)]
 
 for kernel in kernelsizes:
-    blur = cv2.boxFilter(img, -1, kernel)
+    blur = cv2.boxFilter(img, -1, kernel, anchor=(-1,-1), normalize=True, borderType=cv2.BORDER_DEFAULT)
     cv2.imshow('Average : ' + str(kernel), blur)
 
 cv2.waitKey(0)
