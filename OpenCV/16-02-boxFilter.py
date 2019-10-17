30277b53ad41690bdd34b6f117dde0a665708135
king711 <960889@qq.com>
2019-10-17 15:35:35 +0800

    update

---
+++ OpenCV/16-02-boxFilter.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('./res/mini.jpeg')
+cv2.imshow('org', img)
+kernelsizes = [(3,3), (9,9), (15, 15)]
+
+for kernel in kernelsizes:
+    blur = cv2.boxFilter(img, -1, kernel)
+    cv2.imshow('Average : ' + str(kernel), blur)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
